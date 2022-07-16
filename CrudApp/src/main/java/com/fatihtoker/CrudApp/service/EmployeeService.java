36ec0d26d782346b007623a9244b17a2dc5133ff
fatih4242 <fatih16@live.nl>
2022-07-16 20:25:31 +0300

    Feature Delete added

--- CrudApp/src/main/java/com/fatihtoker/CrudApp/service/EmployeeService.java
+++ CrudApp/src/main/java/com/fatihtoker/CrudApp/service/EmployeeService.java
@@ -7,4 +7,6 @@ import com.fatihtoker.CrudApp.model.Employee;
 public interface EmployeeService {
 	List<Employee> getAllEmployees();
 	void saveEmployee(Employee employee);
+	Employee getEmployeeById(long id);
+	void deleteEmplooye(Employee employee);
 }
--- CrudApp/src/main/java/com/fatihtoker/CrudApp/service/EmployeeServiceImpl.java
+++ CrudApp/src/main/java/com/fatihtoker/CrudApp/service/EmployeeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.fatihtoker.CrudApp.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,25 @@ public class EmployeeServiceImpl implements EmployeeService{
 	@Override
 	public void saveEmployee(Employee employee) {
 		this.employeeRepository.save(employee);
+		
+	}
+
+	@Override
+	public Employee getEmployeeById(long id) {
+		Optional<Employee> optional = employeeRepository.findById(id);
+		Employee employee = null;
+		if (optional.isPresent()) {
+			employee = optional.get();
+			
+		}else {
+			throw new RuntimeException("Employee not found for id : "+id);
+		}
+		return employee;
+	}
+
+	@Override
+	public void deleteEmplooye(Employee employee) {
+		this.employeeRepository.delete(employee);
 		
 	}
 
